55f1f6d344641029b1e3c6ae976bfff6e2cecc9b
achilles_xushy <yuqingxushiyin@gmail.com>
2017-06-07 16:54:49 +0800

    检测解析的xml返回的字典是否为空

--- web_server/model/sqlite_query.py
+++ web_server/model/sqlite_query.py
@@ -30,16 +30,20 @@ class SqliteQuery(object):
     def trans_transfer_status_to_percent(tuple_one):
         xml_bytes = tuple_one[6].encode(encoding='utf-8')
         xml_dict = xml_parser.XmlParser.parse_string(xml_bytes)
-        percent_list = []
-        for k, v in xml_dict.items():
-            if 'Output_' in k:
-                pp_str = v.get('percentComplete', '')
-                percent_list.append(str(pp_str))
+        if xml_dict is not None:
+            percent_list = []
+            for k, v in xml_dict.items():
+                if 'Output_' in k:
+                    pp_str = v.get('percentComplete', '')
+                    percent_list.append(str(pp_str))
 
-        percent_str = '-'.join(percent_list)
+            percent_str = '-'.join(percent_list)
 
-        return (tuple_one[0], tuple_one[1], tuple_one[2], tuple_one[3], tuple_one[4], tuple_one[5], percent_str,
-                tuple_one[7], tuple_one[8])
+            return (tuple_one[0], tuple_one[1], tuple_one[2], tuple_one[3], tuple_one[4], tuple_one[5], percent_str,
+                    tuple_one[7], tuple_one[8])
+        else:
+            return (tuple_one[0], tuple_one[1], tuple_one[2], tuple_one[3], tuple_one[4], tuple_one[5], 'None',
+                    tuple_one[7], tuple_one[8])
 
     @staticmethod
     def query_res_table_count(current_page, page_limit=100):
