21efaf19100b75ce4da30483b401758efd82be9a
achilles_xushy <yuqingxushiyin@gmail.com>
2017-09-14 15:37:48 +0800

    全面清理本地数据库与数码不匹配的记录，将本地数据库中，码率错误的记录删除。

--- get_inject_record_from_log.py
+++ get_inject_record_from_log.py
@@ -31,7 +31,7 @@ def get_log_record(log_file):
     need_del_list = []
     assetid_list = []
     if os.path.exists(log_file) and os.path.isfile(log_file):
-        a_set_dict = get_shuma_assetid_from_file()
+        # a_set_dict = get_shuma_assetid_from_file()
         with open(log_file, mode='r', encoding='utf-8') as pf:
             for l_str in pf.readlines():
                 if l_str.startswith('<TransferContent providerID='):
@@ -39,12 +39,12 @@ def get_log_record(log_file):
                     inject_xml_str = '<?xml version=\'1.0\' encoding=\'utf-8\'?>\n{}'.format(r_str)
                     asset_id_str = get_assetid_from_xml(inject_xml_str)
                     assetid_list.append(asset_id_str)
-                    r_list = a_set_dict.get(asset_id_str, [])
-                    if r_list:
-                        delete_str = get_delete_xml_str(asset_id_str, r_list)
-                        need_del_list.append(delete_str)
-                    else:
-                        print(inject_xml_str)
+                    # r_list = a_set_dict.get(asset_id_str, [])
+                    # if r_list:
+                    #     delete_str = get_delete_xml_str(asset_id_str, r_list)
+                    #     need_del_list.append(delete_str)
+                    # else:
+                    #     print(inject_xml_str)
     return need_del_list, assetid_list
 
 
@@ -54,7 +54,7 @@ def get_assetid_from_xml(in_xml_str):
 
 
 def get_shuma_assetid_from_file():
-    file_shuma = 'new_get_shuma.txt'
+    file_shuma = 'new_get_shuma_20170908.txt'
     asset_dict = defaultdict(list)
     ret_dict = dict()
     with open(file_shuma, mode='r') as pf:
@@ -64,11 +64,11 @@ def get_shuma_assetid_from_file():
                 id_rate = s_p[0].split(sep='_')
                 asset_dict[id_rate[0]].append((s_p[1], id_rate[1]))
 
-    # for ki, vi in asset_dict.items():
-    #     res_list = sorted(vi, key=lambda it: datetime.strptime(it[0], '%Y-%m-%d %H:%M:%S'))
-    #     ret_dict[ki] = [p[1] for p in res_list]
-    # return ret_dict
-    return asset_dict
+    for ki, vi in asset_dict.items():
+        res_list = sorted(vi, key=lambda it: datetime.strptime(it[0], '%m/%d/%Y %H:%M:%S'))
+        ret_dict[ki] = [p[1] for p in res_list]
+    return ret_dict, asset_dict
+    # return asset_dict
 
 
 def get_delete_xml_str(in_asset_id, rate_list):
@@ -99,6 +99,13 @@ def get_all_req_xml_in_res_table():
         if i[5] == 3 and i[7] == 1:
             req_xml_str = i[8]
             yield XmlParser.get_xml_assetid_and_subid(req_xml_str.encode(encoding='utf-8'))
+
+
+def get_all_record_in_res_table():
+    all_cnt = sqlite_interface.get_res_table_count()
+    result_tuple_list = sqlite_interface.get_res_table_record_list(0, all_cnt)
+    print('record len <{}>'.format(len(result_tuple_list)))
+    return [i[3] for i in result_tuple_list]
 
 
 def sql_record_delete(set_id_list):
@@ -138,95 +145,84 @@ def delete_shuma_xml_record(input_byte_str):
 
 
 def clear_all_main_entrance():
-    a_dict = get_shuma_assetid_from_file()
+    a_dict, asset_dict = get_shuma_assetid_from_file()
     a_count = 0
     for k, v in a_dict.items():
         a_count += len(v)
-    print('a_count = {}'.format(a_count))
+    # print('a_count = {}, shuma dict len <{}>'.format(a_count, len(a_dict)))
 
     sql_dict = OrderedDict()
     for i in get_all_req_xml_in_res_table():
         sql_dict[i['assetID']] = i['subIDs']
 
+    print('sql len <{}>, shuma - sql = len <{}>'.format(len(sql_dict), len(a_dict) - len(sql_dict)))
+
+    count_shuma1 = 0
     ready_del_dict = dict()
     for k, v in a_dict.items():
         if k not in sql_dict:
+            count_shuma1 += 1
             ready_del_dict[k] = v
     # print('file dict len <{}>'.format(len(a_dict)))
     # print('sql dict len <{}>'.format(len(sql_dict)))
     # print(len(ready_del_dict))
+    count_shuma2 = 0
     sql_del_dict = dict()
     for k, v in sql_dict.items():
         if k not in a_dict:
+            count_shuma2 += 1
             sql_del_dict[k] = v
 
+    print('assetid in shuma not in sql len({}), assetid in sql not in shuma len({})'.format(count_shuma1, count_shuma2))
+
     more_all_list = []
-    more_more_list = []
-    more_rep_list = []
-    more_other_list = []
     diff_list = []
     less_list = []
     sql_more_list = []
     for k, v in sql_dict.items():
         if k not in sql_del_dict and k in a_dict:
             if len(sql_dict[k]) < len(a_dict[k]):
-                for i1 in sql_dict[k]:
-                    if i1 not in a_dict[k] and k not in more_other_list:
-                        more_other_list.append(k)
                 more_all_list.append(k)
-
             elif len(sql_dict[k]) > len(a_dict[k]):
                 less_list.append(k)
             else:
-                for i2 in sql_dict[k]:
-                    if i2 not in a_dict[k] and k not in diff_list:
-                        diff_list.append(k)
+                if len(set(sql_dict[k])) != len(set(a_dict[k])):
+                    diff_list.append(k)
+                else:
+                    for i2 in sql_dict[k]:
+                        if i2 not in a_dict[k]:
+                            diff_list.append(k)
+
         elif k not in a_dict:
             sql_more_list.append(k)
 
     for i in more_all_list:
-        if len(set(sql_dict[i])) != len(sql_dict[i]):
-            more_rep_list.append(i)
-    #         print('sql--{}--{}---<{}>'.format(i, sql_dict[i], a_dict[i]))
-    # print('list len <{}>, set len <{}>'.format(len(more_rep_list), len(set(more_rep_list))))
-
-    for i in more_all_list:
-        if i not in more_rep_list and i not in more_other_list:
-            more_more_list.append(i)
-
-    for i in more_more_list:
-        dif_list_c = list(set(a_dict[i]) - set(sql_dict[i]))
-        ready_del_dict[i] = dif_list_c
-        # print('<{}>-sql-<{}>---<{}>---<{}>'.format(i, sql_dict[i], a_dict[i], dif_list_c))
-    # print('list len <{}>, set len <{}>'.format(len(more_more_list), len(set(more_more_list))))
-
-    for i in more_rep_list:
         ready_del_dict[i] = a_dict[i]
-    # print('list len <{}>, set len <{}>'.format(len(more_rep_list), len(set(more_rep_list))))
+        print(i, sql_dict[i], a_dict[i])
+    print('more list len <{}>, set len <{}>'.format(len(more_all_list), len(set(more_all_list))))
 
-    for i in more_other_list:
-        ready_del_dict[i] = a_dict[i]
-    #     print('other-<{}>-sql-<{}>---<{}>'.format(i, sql_dict[i], a_dict[i]))
-    # print('list len <{}>, set len <{}>'.format(len(more_other_list), len(set(more_other_list))))
     for i in less_list:
         ready_del_dict[i] = a_dict[i]
-    #     print('less key-<{}>-sql-<{}>---<{}>'.format(i, sql_dict[i], a_dict[i]))
-    # print('list len <{}>, set len <{}>'.format(len(less_list), len(set(less_list))))
+        print(i, sql_dict[i], a_dict[i])
+    print('less list len <{}>, set len <{}>'.format(len(less_list), len(set(less_list))))
+
     for i in diff_list:
         ready_del_dict[i] = a_dict[i]
-    #     print('diff key-<{}>-sql-<{}>---<{}>'.format(i, sql_dict[i], a_dict[i]))
-    # print('list len <{}>, set len <{}>'.format(len(diff_list), len(set(diff_list))))
-    sql_del_list = more_rep_list + more_other_list + diff_list + less_list + sql_more_list
-    # print('list len <{}>, set len <{}>'.format(len(sql_del_list), len(set(sql_del_list))))
-    # print('del list dict len is <{}>'.format(len(ready_del_dict)))
+        print(i, sql_dict[i], a_dict[i])
+    print('diff list len <{}>, set len <{}>'.format(len(diff_list), len(set(diff_list))))
+
+    sql_del_list = more_all_list + diff_list + less_list + sql_more_list
+    print('delete sqlite list len <{}>, set len <{}>'.format(len(sql_del_list), len(set(sql_del_list))))
+    print('shuma del list dict len is <{}>'.format(len(ready_del_dict)))
+
     for k, v in ready_del_dict.items():
         byte_xml_str = get_delete_xml_str(k, v)
         time.sleep(0.1)
         d_ret = delete_shuma_xml_record(byte_xml_str)
         if d_ret:
-            print('>>>>>shuma delete {} ok'.format(k))
+            print('>>>>>shuma delete {} ok <{}>'.format(k, v))
         else:
-            print('>>>>>shuma delete {} ng'.format(k))
+            print('>>>>>shuma delete {} ng <{}>'.format(k, v))
     ret_failed_list = mysql_record_delete(sql_del_list)
     need_remove_res_table = list(set(sql_del_list) - set(ret_failed_list))
     sql_record_delete(need_remove_res_table)
@@ -235,22 +231,27 @@ def clear_all_main_entrance():
 
 
 def clear_wrong_rate_record():
+    print('\n\nclear all rate len > 4 ........................\n')
     del_dict = dict()
     sql_del_list = []
-    a_dict = get_shuma_assetid_from_file()
-    for k, v in a_dict.items():
+    sql_dict = OrderedDict()
+    for i in get_all_req_xml_in_res_table():
+        sql_dict[i['assetID']] = i['subIDs']
+
+    for k, v in sql_dict.items():
         if len(v) > 4:
-            del_dict[k] = [i[1] for i in v]
+            del_dict[k] = v
             sql_del_list.append(k)
+            # print(k, v)
 
     for k, v in del_dict.items():
         byte_xml_str = get_delete_xml_str(k, v)
         time.sleep(0.1)
         d_ret = delete_shuma_xml_record(byte_xml_str)
         if d_ret:
-            print('>>>>>shuma delete {} ok'.format(k))
+            print('>>>>>shuma delete {} ok <{}>'.format(k, v))
         else:
-            print('>>>>>shuma delete {} ng'.format(k))
+            print('>>>>>shuma delete {} ng <{}>'.format(k, v))
 
     ret_failed_list = mysql_record_delete(sql_del_list)
     need_remove_res_table = list(set(sql_del_list) - set(ret_failed_list))
@@ -259,8 +260,56 @@ def clear_wrong_rate_record():
     print('we coped with assetid list <{}>'.format(sql_del_list))
 
 
+def query_cid_table():
+    a_list_set = sqlite_interface.get_all_asset_id_from_cid_table()
+    # count_x = 0
+    res_list = []
+    for ix in a_list_set:
+        res_dd = sqlite_interface.get_cid_query_result(ix[1])
+        res_list.append(res_dd['assetid'])
+    #     if res_dd:
+    #         count_x += 1
+    #         print(res_dd)
+    #     else:
+    #         print('{} have error!'.format(ix[1]))
+    # print('find right count <{}>'.format(count_x))
+    # print('cid_table record len <{}>'.format(len(a_list_set)))
+    return res_list
+
 if __name__ == '__main__':
     # d_list, a_list = get_log_record(sys.argv[1])
+    # a_dict = dict()
+    # for i in a_list:
+    #     if i in a_dict:
+    #         a_dict[i] += 1
+    #     else:
+    #         a_dict[i] = 1
+    # aim_str = ''
+    # aim_str1 = ''
+    # for k in a_dict.keys():
+    #     aim_str += 'or sub_cdn_id=\'{}\' '.format(k)
+    #     aim_str1 += ' or asset_id={}'.format(int(k)-100000)
+    # print(aim_str)
+    # print(aim_str1)
+    # print('error assetid len <{}>'.format(len(a_dict)))
+
+    # print('409 error list len <{}>, 409 set len <{}>'.format(len(a_list), len(set(a_list))))
+    # sm_dict = get_shuma_assetid_from_file()
+    # for i in a_list:
+    #     print(i, sm_dict[i])
+
+    # count1 = 0
+    # aim_str = ''
+    # sm_dict = get_shuma_assetid_from_file()
+    # for k, v in sm_dict.items():
+    #     if len(v) > 4:
+    #         count1 += 1
+    #         print(k, v)
+    #         aim_str += 'or sub_cdn_id=\'{}\' '.format(k)
+    # print(count1)
+    # print(aim_str)
+    # print('shuma record len <{}>'.format(len(sm_dict)))
+
     # with open('shuma_delete.txt', mode='w', encoding='utf-8') as wf:
     #     for i in d_list:
     #         wf.write('\"\"\"{}\"\"\",\n'.format(i.decode(encoding='utf-8')))
@@ -276,5 +325,23 @@ if __name__ == '__main__':
     # for k, v in ab_dict.items():
     #     if k not in a_list:
     #         print('still {}-----<{}>'.format(k, v))
-    # clear_all_main_entrance()
+
+    clear_all_main_entrance()
     clear_wrong_rate_record()
+    # sql_list = get_all_record_in_res_table()
+
+    # aim_str1 = ''
+    # extra_list = []
+    # for k, v in sm_dict.items():
+    #     if k not in sql_list:
+    #         extra_list.append(k)
+    #         aim_str1 += ' or asset_id={}'.format(int(k) - 100000)
+    # print(aim_str1)
+    # asset_list = query_cid_table()
+    #
+    # for i in sql_list:
+    #     if i not in asset_list:
+    #         print(i)
+    # print('sqlite len <{}>, cidtable len <{}>'.format(len(sql_list), len(asset_list)))
+
+
